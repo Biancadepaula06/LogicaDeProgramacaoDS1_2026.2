cbf0b471b036aba29450387fcd31b73081284534
Biancadepaula06 <biancadepaulapinheiro7@gmail.com>
2026-09-14 14:50:15 -0300

    aula 01

---
+++ 02_conceitos_basicos/pratica01.py
@@ -0,0 +1,11 @@
+
+nome_produto = "salgadinho"
+quantidade_estoque = 10
+preco_unitario = 2.0
+disponivel_para_venda = True
+
+# TODO: Escreva os prints de inspeção aqui
+print("tipo do produto:", type(nome_produto))
+print("tipo do estoque:", type(quantidade_estoque))
+print("tipo do unitario:", type(preco_unitario))
+print("tipo do disponivel para venda:", type(disponivel_para_venda))
---
+++ 02_conceitos_basicos/pratica02.py
@@ -0,0 +1,9 @@
+# TODO: Desenvolva seu algoritmo aqui
+# 1. Leia o valor da conta (float)
+# 2. Leia o número de pessoas (int)
+# 3. Calcule o valor por pessoa
+# 4. Imprima formatado usando f-string
+valor_conta = float (input("valor da conta"))
+numero_pessoas = int (input("numero de pessoas"))
+valor_pessoas = valor_conta/numero_pessoas 
+print(valor_pessoas,("valor por pessoa"))
